a88e26f963de1c16cb2821fbd561e42f2269f0bd
Grant Polychronis <grantpolychronis@gmail.com>
2021-02-10 21:40:21 -0600

    Update NFL_Analysis.R

--- NFL_Analysis.R
+++ NFL_Analysis.R
@@ -15,3 +15,19 @@ missing_values <- sapply(NFL, function(x) sum(is.na(x)))
 missing_values
 
 count(NFL2009)/count(NFL)
+
+FirstGame <- filter(NFL2009, GameID == 2009091000)
+
+FirstGame <- data.frame(lapply(FirstGame, as.character), stringsAsFactors=FALSE)
+
+typeof(FirstGame$sp)
+typeof(FirstGame$PosTeamScore)
+
+FirstGame$Touchdown <- as.integer(FirstGame$Touchdown)
+FirstGame$sp <- as.integer(FirstGame$sp)
+FirstGame$PosTeamScore <- as.integer(FirstGame$PosTeamScore)
+
+FirstGame <- mutate(FirstGame, 
+                    HomeTeamScore = ifelse(posteam == HomeTeam, PosTeamScore + Touchdown*6 + ifelse(FieldGoalResult == "Good",3,0), DefTeamScore),
+                    AwayTeamScore = ifelse(posteam == AwayTeam, PosTeamScore + Touchdown*6 + ifelse(FieldGoalResult == "Good",3,0), DefTeamScore)
+                    )
