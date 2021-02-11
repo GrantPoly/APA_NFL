03b0a092ec7a133509911fa626bcf1eb29cd2dd9
Grant Polychronis <grantpolychronis@gmail.com>
2021-02-10 19:38:15 -0600

    Update NFL_Analysis.R

--- NFL_Analysis.R
+++ NFL_Analysis.R
@@ -1,3 +1,15 @@
 library(tidyverse)
 
 NFL <- read.csv("Data/NFL Play by Play 2009-2016 (v3).csv", fileEncoding = "UTF-8-BOM")
+
+NFL2009 <- filter(NFL, Season == 2009)
+
+#save(NFL2009, file = "Data/NFL2009.RData")
+
+#load("Data/NFL2009.RData")
+
+missing_values <- sapply(NFL, function(x) sum(is.na(x)))
+
+missing_values
+
+count(NFL2009)/count(NFL)
